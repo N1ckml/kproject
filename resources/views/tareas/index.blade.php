3bf97f205dd40627384de8017e128ba7218835da
N1ckml <jeanml0939@gmail.com>
2024-12-08 06:17:48 +0000

    viewNormalUserAdd

--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/styles.css', 'resources/css/app.css']) <!-- Importar estilos -->
     <title>Gestión de Tareas</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
